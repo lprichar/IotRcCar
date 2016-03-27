9b8d7457d950e7aa74217451b69cf689339cb9d2
lprichar <github@leerichardson.com>
2016-03-27 15:42:17 -0400

    Use embedded resource rather than raw stream

--- IotWebServer/StartupTask.cs
+++ IotWebServer/StartupTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
@@ -79,25 +80,33 @@ namespace IotWebServer
 
         private void WriteResponse(string requestPart, StreamSocket socket)
         {
-            string html = "<html><body>Hello World!</body></html>";
-            byte[] bodyArray = Encoding.UTF8.GetBytes(html);
-            // Show the html 
-            using (var outputStream = socket.OutputStream)
+            WriteResource(socket, "IotWebServer.Default.html");
+        }
+
+        private void WriteResource(StreamSocket socket, string resource)
+        {
+            var assembly = typeof(HttpServer).GetTypeInfo().Assembly;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resource))
             {
-                using (Stream resp = outputStream.AsStreamForWrite())
-                {
-                    using (MemoryStream stream = new MemoryStream(bodyArray))
-                    {
-                        string header = String.Format("HTTP/1.1 200 OK\r\n" +
-                                            "Content-Length: {0}\r\n" +
-                                            "Connection: close\r\n\r\n",
-                                            stream.Length);
-                        byte[] headerArray = Encoding.UTF8.GetBytes(header);
-                        resp.Write(headerArray, 0, headerArray.Length);
-                        stream.CopyTo(resp);
-                        resp.Flush();
-                    }
-                }
+               WriteStream(socket, stream);
+            }
+        }
+
+        private static void WriteStream(StreamSocket socket, Stream sourceStream)
+        {
+            using (var outputStream = socket.OutputStream)
+            using (Stream resp = outputStream.AsStreamForWrite())
+            {
+                string header = String.Format("HTTP/1.1 200 OK\r\n" +
+                                              "Content-Length: {0}\r\n" +
+                                              "Content-Type: text/html; charset=utf-8\r\n" +
+                                              "Connection: close\r\n\r\n",
+                    sourceStream.Length);
+                byte[] headerArray = Encoding.UTF8.GetBytes(header);
+                resp.Write(headerArray, 0, headerArray.Length);
+                sourceStream.CopyTo(resp);
+                resp.Flush();
             }
         }
 
